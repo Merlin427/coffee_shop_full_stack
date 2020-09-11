6547209f4236efe39023cd7482c5255d37c70b32
danievanrensburg@hotmail.com <danievanrensburg@hotmail.com>
2020-09-11 11:47:34 +0200

    Fixed typo in API

--- backend/src/api.py
+++ backend/src/api.py
@@ -39,7 +39,7 @@ def get_drinks():
 
 
 @app.route('/drinks-detail', methods=['GET'])
-@requires_auth('get:drinks-datail')
+@requires_auth('get:drinks-detail')
 def get_drinks_detail(payload):       #pass in payload
     drinks = Drink.query.all()
 
